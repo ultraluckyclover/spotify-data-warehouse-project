95d11df324a6d489895ae6388c46b85f31bdb276
Drew <122467323+ultraluckyclover@users.noreply.github.com>
2025-03-05 20:13:03 -0500

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS bronze.unstructured_data;
+
+CREATE TABLE bronze.unstructured_data(
+	id INT PRIMARY KEY,
+	track_name TEXT,
+	track_album_name TEXT,
+	track_artist TEXT,
+	track_release_year INT,
+	track_popularity INT,
+	playlist_genre TEXT,
+	playlist_subgenre TEXT,
+	energy NUMERIC,
+	tempo NUMERIC,
+	danceability NUMERIC,
+	loudness NUMERIC,
+	liveness NUMERIC,	
+	valence NUMERIC,
+	speechiness NUMERIC,
+	instrumentalness NUMERIC,
+	acousticness NUMERIC,
+	mode INT,
+	time_signature INT,
+	key INT,
+	duration_ms INT
+);
