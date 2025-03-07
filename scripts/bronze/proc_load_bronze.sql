fe456f79823223b70cdf6f362fa904c86b8d6d3d
Drew <122467323+ultraluckyclover@users.noreply.github.com>
2025-03-07 12:59:59 -0500

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,17 +1,17 @@
 -- This load procedure works as long as your csv file is accessible to the PostgreSQL server.
 -- Otherwise, load from the client side in psql using \copy or using pgAdmin's import tool.
 
-CREATE OR REPLACE PROCEDURE bronze.load_bronze
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-	-- Clear data from table
-	TRUNCATE TABLE bronze.unstructured_data;
-	
-	-- Load data to table
-	COPY bronze.unstructured_data
-	FROM '/path/to/your/file.csv'
-	DELIMITER ','
-	CSV HEADER;
+    -- Clear data from table
+    TRUNCATE TABLE bronze.unstructured_data;
+
+    -- Load data to table
+    COPY bronze.unstructured_data
+    FROM '/path/to/your/file.csv'
+    DELIMITER ','
+    CSV HEADER;
 END;
 $$;
