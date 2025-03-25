1d85b14e1faeb6cf901322a41324b1efefe38818
Drew <122467323+ultraluckyclover@users.noreply.github.com>
2025-03-25 12:36:18 -0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,3 +1,4 @@
+-- ENTITY TABLES
 INSERT INTO silver.artists (artist_name)
 SELECT DISTINCT TRIM(UNNEST(STRING_TO_ARRAY(track_artist, ','))) AS artist_name
 FROM bronze.unstructured_data;
@@ -54,3 +55,18 @@ b.duration_ms
 FROM bronze.unstructured_data b
 JOIN silver.albums a ON b.track_album_name = a.album_name
 ON CONFLICT DO NOTHING;
+
+-- JUNCTION TABLES
+
+CREATE TEMPORARY TABLE temp_artists AS 
+SELECT 
+track_name, 
+TRIM(UNNEST(STRING_TO_ARRAY(track_artist, ','))) AS raw_artist_name
+FROM bronze.unstructured_data;
+
+INSERT INTO silver.track_artist (track_id, artist_id)
+SELECT t.track_id, a.artist_id
+FROM temp_artists ta
+JOIN silver.tracks t ON ta.track_name = t.track_name
+JOIN silver.artists a ON ta.raw_artist_name = a.artist_name
+ON CONFLICT (track_id, artist_id) DO NOTHING;
